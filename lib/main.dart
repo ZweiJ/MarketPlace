63053c878b7fb50a2596a43db83d81e7ebaa32e4
Abraham Julian <abrahamjulian11@gmail.com>
2023-09-20 03:01:08 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:ecommerce/constants.dart';
 import 'package:ecommerce/routes.dart';
-import 'package:ecommerce/screens/sign_in/sign_in_screen.dart';
 import 'package:ecommerce/screens/splash/splash_screen.dart';
 import 'package:ecommerce/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 
 void main() {
   runApp(const MyApp());
